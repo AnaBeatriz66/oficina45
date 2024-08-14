185513b354eb3abd2851d71e91483cf2263961ee
AnaBeatriz66 <145566553+AnaBeatriz66@users.noreply.github.com>
2024-08-14 14:42:49 -0300

    comecei

---
+++ oficina1/Assets/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+
+
+       public float Speed;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moviment = new Vector3(Input.GetAxis("Horizontal")0f,0f)
+    }
+}
