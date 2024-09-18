4be2bd17fcf19be244a16e33c38b5c4158146b5d
AnaBeatriz66 <145566553+AnaBeatriz66@users.noreply.github.com>
2024-09-18 16:34:55 -0300

    fim....

--- oficina1/Assets/Scenes/EfeitoDigitador.cs
+++ oficina1/Assets/Scenes/EfeitoDigitador.cs
@@ -12,31 +12,52 @@ public class EfeitoDigitador : MonoBehaviour
    private AudioSource _audioSource;
    private string mensagemOriginal;
    public bool imprimindo;
+   public float tempoEntreLetras = 0.08f;
 
    private void Awake()
    {
-       
+       TryGetComponent(out componenteTexto);
+       TryGetComponent(out _audioSource);
+       mensagemOriginal = componenteTexto.text;
+       componenteTexto.text = "";
    }
 
    private void OnEnable()
    {
-      
+      imprimirMensagem (mensagemOriginal);
    }
 
    private void OnDisable()
    {
-     
+     componenteTexto.text = mensagemOriginal;
+     StopAllCoroutines();
       
    }
 
-   public imprimirMensagem(string msg)
+    public void imprimirMensagem(string mensagem)
    {
-      
+      if (gameObject.activeInHierarchy)
+      {
+           if (imprimindo) return;
+           imprimindo = true;
+           StartCoroutine(LetraporLetra(mensagem));
+      }
+
    }
 
-   IEnumerator LetraporLetra(string msg)
+   IEnumerator LetraporLetra(string mensagem)
+  
    {
-      yield return null;
+       string msg = " ";
+     foreach (var letra in mensagem)
+     {
+         msg += letra;
+         componenteTexto.text = msg;
+         _audioSource.Play();
+          yield return new WaitForSeconds(tempoEntreLetras);
+     }
+     imprimindo = false;
+     StopAllCoroutines();
    }
 }
 
